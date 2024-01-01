6df878ee49c6785cfdcd5dcf03774fa5eab7cdff
Elaidzha1940 <eli080293@gmail.com>
2024-01-01 17:49:31 +0300

    AsyncAwait

---
+++ AsyncAwait/AsyncAwait.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: AsyncAwait
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 01.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct AsyncAwait: View {
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    AsyncAwait()
+}
---
+++ AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: AsyncAwait
+//  File: AsyncAwaitApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 01.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AsyncAwait()
+        }
+    }
+}
