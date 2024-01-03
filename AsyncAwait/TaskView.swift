185c86566e641e2d1736d927120e546cfd6c0cd3
Elaidzha1940 <eli080293@gmail.com>
2024-01-03 03:29:10 +0300

    Update TaskView

--- AsyncAwait/TaskView.swift
+++ AsyncAwait/TaskView.swift
@@ -11,9 +11,26 @@ import SwiftUI
 
 class TaskViewViewModel: ObservableObject {
     @Published var image: UIImage? = nil
+    @Published var image2: UIImage? = nil
     
-    func fetchImage() {
-        
+    func fetchImage() async {
+        do {
+            guard let url = URL(string: "https://source.unsplash.com/random/300×400") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            self.image = UIImage(data: data)
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchImage2() async {
+        do {
+            guard let url = URL(string: "https://source.unsplash.com/random/300×400") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            self.image2 = UIImage(data: data)
+        } catch  {
+            print(error.localizedDescription)
+        }
     }
 }
 
@@ -22,8 +39,64 @@ struct TaskView: View {
     
     var body: some View {
         
-        VStack {
+        VStack(spacing: 20) {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(15 )
+                    .frame(width: 300, height: 300)
+            }
             
+            if let image = viewModel.image2 {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(15 )
+                    .frame(width: 300, height: 300)
+            }
+        }
+        .onAppear {
+//            Task {
+//                print(Thread.current)
+//                print(Task.currentPriority)
+//                await viewModel.fetchImage()
+//            }
+//            Task {
+//                print(Thread.current)
+//                print(Task.currentPriority)
+//                await viewModel.fetchImage2()
+//            }
+        
+//            Task(priority: .high) {
+//               //try? await Task.sleep(nanoseconds: 2_000_000_000)
+//                await Task.yield()
+//                print("high : \(Thread.current) : \(Task.currentPriority)")
+//            }
+//            Task(priority: .userInitiated) {
+//                print("userInitiated : \(Thread.current) : \(Task.currentPriority)")
+//            }
+//            Task(priority: .medium) {
+//                print("medium : \(Thread.current) : \(Task.currentPriority)")
+//            }
+//            Task(priority: .low) {
+//                print("low : \(Thread.current) : \(Task.currentPriority)")
+//            }
+//            Task(priority: .utility) {
+//                print("utility : \(Thread.current) : \(Task.currentPriority)")
+//            }
+//            Task(priority: .background) {
+//                print("background : \(Thread.current) : \(Task.currentPriority)")
+//            }
+            
+//            Task(priority: .userInitiated) {
+//                print("userInitiated : \(Thread.current) : \(Task.currentPriority)")
+//                
+//                Task.detached  {
+//                    print("userInitiated : \(Thread.current) : \(Task.currentPriority)")
+//                }
+//            }
+        
         }
     }
 }
