4de28d41c36c82b50f8a0f0bb35ffa5fe669247f
Elaidzha1940 <eli080293@gmail.com>
2024-01-03 05:37:07 +0300

    Add func fetchImage()

--- AsyncAwait/AsyncLetView.swift
+++ AsyncAwait/AsyncLetView.swift
@@ -10,17 +10,42 @@
 import SwiftUI
 
 struct AsyncLetView: View {
-    @State private var image: [UIImage] = []
+    @State private var images: [UIImage] = []
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let url = URL(string: "https://source.unsplash.com/random/300×400")!
     
     var body: some View {
         
         NavigationStack {
             ScrollView {
                 LazyVGrid(columns: columns) {
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("Async Let 😎")
+            .onAppear {
+                Task {
                     
                 }
             }
+        }
+    }
+    
+    func fetchImage() async throws -> UIImage {
+        do {
+            let (data, _) =  try await URLSession.shared.data(from: url, delegate: nil)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
         }
     }
 }
