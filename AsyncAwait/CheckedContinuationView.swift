497f41719aca76dedf9bb9006a3ed16ee75dd753
Elaidzha1940 <eli080293@gmail.com>
2024-01-04 05:10:53 +0300

    Update CheckedContinuationNetworkManager

--- AsyncAwait/CheckedContinuationView.swift
+++ AsyncAwait/CheckedContinuationView.swift
@@ -11,13 +11,44 @@ import SwiftUI
 
 class CheckedContinuationNetworkManager {
     
+    func getData(url: URL) async throws -> Data {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            return data
+        } catch {
+            throw error
+        }
+    }
+    
+    func getData2(url: URL) async throws -> Data {
+       return try await withCheckedContinuation { continuation in
+           URLSession.shared.dataTask(with: url) { data, response, error in
+               if let data = data {
+                   continuation.resume(returning: data)
+               }
+           }
+        }
+    }
 }
 
 class CheckedContinuationViewModel: ObservableObject {
     @Published var image: UIImage? = nil
+    let networkManager = CheckedContinuationNetworkManager()
     
     func getImage() async {
+        guard let url = URL(string: "https://source.unsplash.com/random/300×400") else { return }
         
+        do {
+           let data = try await networkManager.getData(url: url)
+            
+            if let image = UIImage(data: data) {
+                await MainActor.run {
+                    self.image = image
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
 }
 
@@ -31,6 +62,7 @@ struct CheckedContinuationView: View {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFit()
+                    .cornerRadius(15)
                     .frame(width: 200, height: 300)
             }
         }
